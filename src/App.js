c8ef922298d8db7dbc6059f910d14a9cec244828
Ifeanyi <chimaifeanyi29@gmail.com>
2022-03-30 10:10:09 +0100

    when component renders make API call

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Card from "./components/Card"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import './App.css';
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import axios from "axios"
 
 
@@ -36,6 +36,27 @@ function App() {
     backgroundPosition: `center`,
     backgroundSize: `cover`
   }
+
+  useEffect(() => {
+    const getWeather = async (city) => {
+
+    if(!city) return
+
+      try {
+      const apiKey = "41ade4ff907582d51d1618d3c47281e8"
+      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    
+      setResult(data)
+      console.log(data)
+
+      return data
+    } catch(err){
+      console.log(err.message)
+    }
+  }
+
+    getWeather("denver")
+  }, [])
  
 
   return (
