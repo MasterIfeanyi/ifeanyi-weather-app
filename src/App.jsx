17163c31f79a6af846d0946a3f02a192a27a647e
Ifeanyi <chimaifeanyi29@gmail.com>
2022-05-24 15:16:17 +0100

    total re-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Card from "./components/Card"
 import Header from "./components/Header"
+import Card from "./components/Card"
 import Footer from "./components/Footer"
 import './App.css';
 import { useState, useEffect } from "react"
@@ -9,22 +9,25 @@ import axios from "axios"
 function App() {
 
 
-  const [result, setResult] = useState({}) 
+  const [result, setResult] = useState({})
   const [city, setCity] = useState("")
 
-  
+
   const getWeather = async () => {
-      try {
+
+    if (!city) return
+
+    try {
       const apiKey = "41ade4ff907582d51d1618d3c47281e8"
       const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-    
+
       setResult(data)
-    } catch(err){
+    } catch (err) {
       console.log(err.message)
     }
   }
-  
- 
+
+
   const cardStyle = {
     backgroundImage: `url(https://source.unsplash.com/1600x900/?${result.name})`,
     backgroundPosition: `center`,
@@ -34,32 +37,30 @@ function App() {
   useEffect(() => {
     const getWeather = async (city) => {
 
-    if(!city) return
+      if (!city) return
 
       try {
-      const apiKey = "41ade4ff907582d51d1618d3c47281e8"
-      const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-    
-      setResult(data)
-    } catch(err){
-      console.log(err.message)
+        const apiKey = "41ade4ff907582d51d1618d3c47281e8"
+        const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+
+        setResult(data)
+      } catch (err) {
+        console.log(err.message)
+      }
     }
-  }
 
     getWeather("denver")
   }, [])
- 
+
 
   return (
-    <>
-      <div className="App">
-        <Header />
-        <section className="" style={(typeof result !== "undefined") ? cardStyle : null}>
-          <Card getWeather={getWeather} city={city} setCity={setCity} result={result} />
-        </section>
-        <Footer />
-      </div>
-    </>
+    <main className="App">
+      <Header />
+      <section className="section" style={(typeof result !== "undefined") ? cardStyle : null}>
+        <Card getWeather={getWeather} city={city} setCity={setCity} result={result} />
+      </section>
+      <Footer />
+    </main>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { FaSearch } from "react-icons/fa"
 
-const Card = ({ city, setCity, result, getWeather}) => {
+const Card = ({ city, setCity, result, getWeather }) => {
 
     // const { name } = result
     // const { temp, humidity } = result.main
@@ -9,51 +10,56 @@ const Card = ({ city, setCity, result, getWeather}) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if(!city) return
         await getWeather();
         setCity("")
         return
     }
 
-    
 
-    const textStyle = {
-        color: `white`,
-        fontSize: `1rem`,
-        display: `inline-flex`,
-        alignItems: `center`,
-        justifyContent: `center`,
-        fontWeight: `500`
-    }
+
+    // const textStyle = {
+    //     color: `white`,
+    //     fontSize: `1rem`,
+    //     display: `inline-flex`,
+    //     alignItems: `center`,
+    //     justifyContent: `center`,
+    //     fontWeight: `500`
+    // }
 
     return (
-      <>
-        <div className="card">
-            <form onSubmit={handleSubmit} className="search">
-            <input placeholder="search for a city" value={city} onChange={(e) => setCity(e.target.value)}  type="text" className="search-bar" />
-                <button className='' type="submit">
-                <svg className='' stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path></svg>
-            </button>
-            </form>
-            {/* <img src="https://openweathermap.org/img/wn/04n.png" alt="" class="icon" /> */}
-                {(typeof result.main !== "undefined") ? (
-                <>
-                    <div className="weather loading">
-                        <h2 className="city">Weather in {result.name}</h2>
-                        <h1 className="temp" style={textStyle}>{Math.round(result.main.temp)}°C</h1>
-                        <div className="d-flex">
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-lg-6">
+                    <div className="cardStyle">
+                        <form onSubmit={handleSubmit} className="row">
+                            <div className="form-group col-12 d-flex mb-2">
+                                <input placeholder="search for a city" value={city} onChange={(e) => setCity(e.target.value)} type="text" className="form-control search-bar me-2" />
+                                <button className='btn btn-primary' type="submit">
+                                    <FaSearch />
+                                </button>
+                            </div>
+                        </form>
+                        {/* <img src="https://openweathermap.org/img/wn/04n.png" alt="" class="icon" /> */}
+                        {(typeof result.main !== "undefined") ? (
+                            <>
+                                <div className="weather loading">
+                                    <h3 className="city">Weather in {result.name}</h3>
+                                    <div className="temp"><label htmlFor="" className="label">Temperature:</label> {Math.round(result.main.temp)}°C</div>
+                                    <div className="d-flex">
 
-                            <img src={`https://openweathermap.org/img/wn/${result.weather[0].icon}.png`} alt="" className="icon" />
-                            <div className="description">{result.weather[0].description}</div>
-                        </div>
-                        <div className="humidity">Humidity: {result.main.humidity}%</div>
-                        <div className="wind">Wind speed: {result.wind.speed} km/hr</div>
+                                        <img src={`https://openweathermap.org/img/wn/${result.weather[0].icon}.png`} alt="" className="icon" />
+                                        <span className="description">{result.weather[0].description}</span>
+                                    </div>
+                                    <div className="humidity"><label className="label">Humidity:</label> {result.main.humidity}%</div>
+                                    <div className="wind"><label htmlFor="" className="label">Wind speed:</label> {result.wind.speed} km/hr</div>
+                                </div>
+                            </>
+                        ) : ("")}
                     </div>
-                </>
-            ) : ("")}
+                </div>
+            </div>
         </div>
-      </>
-  )
+    )
 }
 
 export default Card
